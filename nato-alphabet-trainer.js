96c865db17b2bcf5a7398cfae9832b788732942c
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2018-08-21 08:06:15 +0200

    2

--- nato-alphabet-trainer.js
+++ nato-alphabet-trainer.js
@@ -21,4 +21,4 @@ function nato-alphabet-trainer() {
 
 
 
-var natocode = ["A", "Alpha", "B", "Bravo", "C", "Charlie", "D", "Delta", "E", "Echo", "F", "Foxtrot", "G", "Golf", "H", "Hotel", "I", "India", "J", "Juliet",  ];
+var natocode = ["A", "Alpha", "B", "Bravo", "C", "Charlie", "D", "Delta", "E", "Echo", "F", "Foxtrot", "G", "Golf", "H", "Hotel", "I", "India", "J", "Juliet", "K", "Kilo", "L", "Lima", "M", "Mike", "N", "November", "O", "Oscar", "P", "Papa", "Q", "Quebec", "R", "Romeo", "S", "Sierra", "T", "Tango", "U", "Uniform", "V", "Victor", "W", "Whiskey", "X", "X-ray", "Y", "Yankee", "Z", "Zulu"];
