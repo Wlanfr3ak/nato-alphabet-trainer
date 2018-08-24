7949dbb07a25a7b228693338cc313f500e0a7857
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2018-08-24 19:43:45 +0200

    TestCode added

--- nato-alphabet-trainer.js
+++ nato-alphabet-trainer.js
@@ -23,3 +23,17 @@ function nato-alphabet-trainer() {
 
 var natocode = ["A", "Alpha", "B", "Bravo", "C", "Charlie", "D", "Delta", "E", "Echo", "F", "Foxtrot", "G", "Golf", "H", "Hotel", "I", "India", "J", "Juliet", "K", "Kilo", "L", "Lima", "M", "Mike", "N", "November", "O", "Oscar", "P", "Papa", "Q", "Quebec", "R", "Romeo", "S", "Sierra", "T", "Tango", "U", "Uniform", "V", "Victor", "W", "Whiskey", "X", "X-ray", "Y", "Yankee", "Z", "Zulu"];
 var natocode = ["A", "Anton", "Ä", "Ärger", "B", "Berta", "C", "Cäsar", "Ch", "Charlotte", "D", "Dora", "E", "Emil", "F", "Friedrich", "G", "Gustav", "H", "Heinrich", "I", "Ida", "J", "Julius", "K", "Kaufmann", "L", "Ludwig", "M", "Martha", "N", "Nordpol", "O", "Otto", "Ö", "Ökonom", "P", "Paula", "Q", "Quelle", "R", "Richard", "S", "Samuel", "Sch", "Schule", "ß", "Eszett", "T", "Theodor", "U", "Ulrich", "V", "Viktor", "W", "Wilhelm", "X", "Xanthippe", "Y", "Ypsilon", "Z", "Zacharias"];
+
+//benbe code:
+var alphabet = {
+    "a" : "alpha",
+    "b" : "bravo";
+    
+var str = "DO9XE";
+var result = " ";
+
+for(i = 0; i < str.length; i++) {
+    result += " ";
+    result += alphabet[str[i]];
+}
+document.getElementById("resultDiv").innerText = result;
